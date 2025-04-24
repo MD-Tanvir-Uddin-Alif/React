3c70a48e7452357ae8e0556cc8e927ed7e8400d0
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-04-24 23:15:07 +0600

    same

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,28 @@ import React, { useRef, useState } from 'react';
 import Hero from './component/Hero'
 
 const App = () => {
-  const [number, setsumber] = useState(0);
+  const [newOBJ, setOBJ] = useState({
+    key1:"value 1",
+    key2:"value 2",
+    key3:"value 3",
+  });
+
+  const change = ()=>{
+    setOBJ(
+      preobj =>({
+        ...preobj,
+        key1:"new value 1",
+        key2:"new value 2",
+      })
+    )
+  }
 
   return (
     <div>
-      <h1>number:{number}</h1>
-      <button onClick={()=>setsumber(number+1)}>click me</button>
+      <h1>New Object:{newOBJ.key1}</h1>
+      <h1>New Object:{newOBJ.key2}</h1>
+      <h1>New Object:{newOBJ.key3}</h1>
+      <button onClick={change}>click me</button>
     </div>
   );
 };
