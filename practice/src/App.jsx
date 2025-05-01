fc4a69bab52483eae957add3bf1cffb98f4b206e
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-01 23:37:48 +0600

    practicing

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -10,12 +10,16 @@ function App() {
       [...list, userInput.current.value]
     );
   };
+
+
+
   return (
     <>
       <div>
         <input type="text" name="" id="" ref={userInput}/>
         <button onClick={change}>click me</button>
         <p>the input you provided : {list}</p>
+        <h1>Hii</h1>
       </div>
     </>
   )
