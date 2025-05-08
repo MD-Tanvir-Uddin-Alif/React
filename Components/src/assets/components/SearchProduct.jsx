4d7acabc196f150931d9af855ed6a8f756872877
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-08 14:29:53 +0600

    search product creating

---
+++ Components/src/assets/components/SearchProduct.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const SearchProduct = () => {
+
+    const [query, setQuery] = useState('');
+    const [product, setProduct] = useState([]);
+
+    
+
+
+  return (
+    <div></div>
+  )
+}
+
+export default SearchProduct
