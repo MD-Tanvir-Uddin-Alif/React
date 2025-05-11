a1de6c135e03257dd73237cf6dfba5e6ae923524
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-05-11 17:15:01 +0600

    searchProduct component added

--- Components/src/assets/components/About.jsx
+++ Components/src/assets/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ProductList from './ProductList'
 import { useParams } from 'react-router-dom'
 import ProductDetails from './ProductDetails'
+import SearchProduct from './SearchProduct'
 
 const About = () => {
 
@@ -9,6 +10,7 @@ const About = () => {
 
   return (
     <div>
+        <SearchProduct/>
         {id? <ProductDetails/>:<ProductList/>}
     </div>
   )
--- Components/src/assets/components/SearchProduct.jsx
+++ Components/src/assets/components/SearchProduct.jsx
@@ -1,7 +1,36 @@
+import axios from 'axios';
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-export const SearchProduct = () => {
+const SearchProduct = () => {
+
+  const [query, setQuery] = useParams('');
+  const [products, setProducts] = useParams([]);
+
+  const handleSearch = async (e)=>{
+    e.preventDefault();
+    const res = await axios.get(`https://dummyjson.com/products/search?q=${query}`);
+    setProducts(res.data.products);
+  }
+
   return (
-    <div>SearchProduct</div>
+    <div>
+      <form onSubmit={handleSearch} className="flex gap-2">
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="border px-4 py-2 w-full rounded"
+        />
+        <button type="submit" className="bg-green-500 text-white px-4 rounded">Search</button>
+      </form>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+        {products.map(product => <ProductCard key={product.id} product={product} />)}
+      </div>
+    </div>
   )
 }
+
+export default SearchProduct
